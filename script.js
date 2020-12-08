30c0d146ff3bda65aabf913c489c4f3a92e4541f
Willdooo <72411792+Willdooo@users.noreply.github.com>
2020-12-08 21:17:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 
+class book {
+    constructor(title, author, pages, status) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.status = status
+        this.info = function () {
+            return(title + ' by ' + author + ', ' + pages + ' pages' +', ' + status)
+        }
+    }
+}
+const book1 = new book('Pán Prstenu', 'Tolkien', 300, 'nečteno')
+console.log(book1.info())
+
+/**
 function book(title, author, pages, status) {
     this.title = title
     this.author = author
@@ -8,5 +23,4 @@ function book(title, author, pages, status) {
         return(title + ' by ' + author + ', ' + pages + ' pages' +', ' + status)
     }
 }
-const book1 = new book('Pán Prstenu', 'Tolkien', 300, 'nečteno')
-console.log(book1.info())
+ */
